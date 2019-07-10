8be6de2aeca1976485ec8c9697d920df6712b0ef
umadevic <51950421+umadevic@users.noreply.github.com>
2019-07-10 17:16:50 +0530

    Update hun13.py

--- hun13.py
+++ hun13.py
@@ -1,5 +1,5 @@
-m3,n3=map(int,input().split())
-kk3=[]
+q,w1=map(int,input().split())
+ll1=[]
 s1=[]
 a1=[int(m) for m in input().split() ]
 for i in range(0,n1):
@@ -7,7 +7,7 @@ for i in range(0,n1):
     for j in range(u1-1 ,v1):
         s1.append(a1[j])
     x1=sorted(s1)
-    kk3.append(min(s1))
+    ll1.append(min(s1))
     del s1[:]
-for z in range(0,len(kk3)):
-    print(kk3[z])
+for z in range(0,len(ll1)):
+    print(ll[z])
